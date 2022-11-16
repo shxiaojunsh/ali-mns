9a26684238bd1575d5023efbdc759a5f602ea6f9
Xiaojun Shen <shen.xiaojun@outlook.com>
2022-11-16 16:32:08 +0800

    release for alicloud-mns:2.6.11

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var gitVersion={"branch":"master","rev":"138","hash":"b208a1c","hash160":"b208a1c2da2f0259bd60209d32117e8dca72405e"};
+var gitVersion={"branch":"master","rev":"140","hash":"f4c5628","hash160":"f4c5628ba567cbebdfe4151db4749a607c2f00b2"};
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
         ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
